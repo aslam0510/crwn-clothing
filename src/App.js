74a8901f6abeecb6e2a48941123c24a29f382218
Aslam Khan <aslam51096@gmail.com>
2021-11-07 23:42:39 +0530

    update menuitem navigation with router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Route,Switch} from "react-router-dom";
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
-
 
 
 function App() {
   return (
     <div>
-      <HomePage />
+     <Switch>
+      <Route exact path="/" component={HomePage}/>
+     </Switch>
     </div>
   );
 }
--- src/components/Menu-Item/MenuItemComponent.js
+++ src/components/Menu-Item/MenuItemComponent.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React  from 'react'
 import './MenuItem.style.scss';
+import { withRouter } from 'react-router-dom';
 
-export default function MenuItemComponent({ title, imageUrl, size }) {
+ function MenuItemComponent({ title, imageUrl, size,history,match,linkUrl}) {
+  
+  const onCardClick = () =>{
+    console.log(match);
+    console.log(history);
+    console.log('clicked');
+    history.push(`${match.url}${linkUrl}`)
+  }
+
   return (
     <>
-      <div className={`${size} menu-item`}>
+      <div className={`${size} menu-item`} onClick={onCardClick}>
         <div
           className="background-image"
-          style={{ backgroundImage: `url(${imageUrl})` }}
+          style={{ backgroundImage: `url(${imageUrl})`}}
         ></div>
         <div className="content">
           <h1 className="title">{title}</h1>
@@ -17,3 +26,5 @@ export default function MenuItemComponent({ title, imageUrl, size }) {
     </>
   );
 }
+
+export default withRouter(MenuItemComponent)
